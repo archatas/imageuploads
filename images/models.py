0dc73ebd5bfde4adecad2c77c0c0acbe6c8801f6
Aidas Bendoraitis <aidasbend@yahoo.com>
2013-09-19 03:05:56 +0200

    image path containing pk

--- images/models.py
+++ images/models.py
@@ -2,9 +2,14 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
+def upload_to(instance, filename):
+    return '/'.join(['images', unicode(instance.pk), filename])
+
+
 class Image(models.Model):
     title = models.CharField(_("Title"), max_length=200)
-    image = models.ImageField(_("Image"), upload_to="images/")
+    image = models.ImageField(_("Image"), upload_to=upload_to)
 
     class Meta:
         verbose_name = _("Image")
